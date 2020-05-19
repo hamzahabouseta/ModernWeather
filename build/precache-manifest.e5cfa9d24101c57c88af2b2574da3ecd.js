ff10d317a8f306e5ee3fd6f29dc08015d0a10083
Hamzah Abouseta <55253746+hamzahabouseta@users.noreply.github.com>
2020-05-18 20:09:05 -0400

    Add files via upload

---
+++ build/precache-manifest.e5cfa9d24101c57c88af2b2574da3ecd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515337283908066384f433db25a1fb95",
+    "url": "/ModernWeather/index.html"
+  },
+  {
+    "revision": "f1cac3e114eea1509f95",
+    "url": "/ModernWeather/static/css/main.ded72340.chunk.css"
+  },
+  {
+    "revision": "1094bec9646515b40772",
+    "url": "/ModernWeather/static/js/2.71028041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ModernWeather/static/js/2.71028041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1cac3e114eea1509f95",
+    "url": "/ModernWeather/static/js/main.4f17c43b.chunk.js"
+  },
+  {
+    "revision": "161d87f85f16d891c1ee",
+    "url": "/ModernWeather/static/js/runtime-main.39ac2a3d.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/ModernWeather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/ModernWeather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
